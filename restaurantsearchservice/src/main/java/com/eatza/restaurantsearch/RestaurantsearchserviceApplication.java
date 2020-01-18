b6a083677b5718321f7e9c1f4adfd5b1109d05bb
Jobin Thomas <jobinthomas1610@gmail.com>
2020-01-18 06:00:30 +0000

    Bootstrapped properties and config server details for restaurant service. Enabled Eureka Client on restaurantservice

--- restaurantsearchservice/src/main/java/com/eatza/restaurantsearch/RestaurantsearchserviceApplication.java
+++ restaurantsearchservice/src/main/java/com/eatza/restaurantsearch/RestaurantsearchserviceApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import com.eatza.restaurantsearch.config.JwtFilter;
 
 @SpringBootApplication
 @EnableCaching
+@EnableEurekaClient
 public class RestaurantsearchserviceApplication {
 
 	public static void main(String[] args) {
