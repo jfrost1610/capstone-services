bf96d964977dd4d7719344a87cce71b5b0cbea7f
Jobin Thomas <jobinthomas1610@gmail.com>
2020-01-21 06:55:05 +0000

    Added more junit

--- orderingservice/src/test/java/com/eatza/orderingservice/service/ItemServiceTest.java
+++ orderingservice/src/test/java/com/eatza/orderingservice/service/ItemServiceTest.java
@@ -1,6 +1,8 @@
 package com.eatza.orderingservice.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
@@ -16,25 +18,31 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.eatza.order.client.MenuItemFeignClient;
+import com.eatza.order.dto.ItemFetchDto;
 import com.eatza.order.model.Order;
 import com.eatza.order.model.OrderedItem;
 import com.eatza.order.repository.OrderedItemRepository;
 import com.eatza.order.service.ItemService;
 
+import feign.FeignException;
+
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class ItemServiceTest {
-	
+
 	@Mock
 	OrderedItemRepository itemRepository;
-	
+
 	@InjectMocks
 	ItemService itemServiceImpl;
-	
-	
+
+	@Mock
+	MenuItemFeignClient menuItemFeignClient;
+
 	@Test
 	public void saveItem_basic() {
-		Order order  = new Order(1L, "UPDATED", 1L);
+		Order order = new Order(1L, "UPDATED", 1L);
 		OrderedItem orderedItem = new OrderedItem();
 		orderedItem.setName("Dosa");
 		orderedItem.setId(1L);
@@ -47,27 +55,32 @@ public class ItemServiceTest {
 		assertEquals(order, returnedItem.getOrder());
 		assertEquals(2, returnedItem.getQuantity());
 	}
-	
-	
+
 	@Test
 	public void findByOrderId_basic() {
-		Order order  = new Order(1L, "UPDATED", 1L);
+		Order order = new Order(1L, "UPDATED", 1L);
 		OrderedItem orderedItem = new OrderedItem("Dosa", 2, 100, order, 1L);
 		List<OrderedItem> orderedItems = new ArrayList<>();
 		orderedItems.add(orderedItem);
 		when(itemRepository.findbyOrderId(anyLong())).thenReturn(orderedItems);
-		List<OrderedItem> items =  itemServiceImpl.findbyOrderId(1L);
-		assertEquals("Dosa",items.get(0).getName());
-		
+		List<OrderedItem> items = itemServiceImpl.findbyOrderId(1L);
+		assertEquals("Dosa", items.get(0).getName());
+
 	}
-	
+
 	@Test
 	public void deleteItemsById() {
-		
+
 		Mockito.doNothing().when(itemRepository).deleteById(anyLong());
 		itemServiceImpl.deleteItemsById(1L);
 	}
-	
-	
+
+	@Test
+	public void findItemById() {
+
+		Mockito.when(menuItemFeignClient.getItemById(anyLong())).thenReturn(new ItemFetchDto());
+		ItemFetchDto result = itemServiceImpl.findItemById(1L);
+		assertNotNull(result);
+	}
 
 }
